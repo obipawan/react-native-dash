9165714ae426d10201f4ef349ff2cace903fd3d6
Pawan <pawan.kumar1@myntra.com>
2016-05-29 18:29:37 +0530

    initial commit ¯\_(ツ)_/¯

---
+++ Dash.js
@@ -0,0 +1,54 @@
+/*
+* Draws fully customizable dashed lines vertically or horizontally
+*
+* @providesModule Dash
+*/
+'use strict';
+
+import React from 'react';
+import {View} from 'react-native';
+
+const Dash = ({length, dashGap, dashLength, dashThickness, dashColor:backgroundColor, style}) => {
+  let {flexDirection = 'row'} = style;
+  let width = dashLength;
+  let height = dashThickness;
+  let marginRight = dashGap;
+  let marginBottom = 0;
+  if (flexDirection === 'column'){
+    width = dashThickness;
+    height = dashLength;
+    marginRight = 0;
+    marginBottom = dashGap;
+  }
+
+  let n = Math.ceil(length / (dashGap + dashLength));
+  let dash = [];
+  for (let i = 0; i < n; i++){
+    dash.push(
+      <View key={i} style={{backgroundColor, width, height, marginRight, marginBottom}}/>
+    );
+  }
+  return (
+    <View style={[style, {flexDirection}]}>
+      {dash}
+    </View>
+  )
+};
+
+Dash.propTypes = {
+  style: View.propTypes.style,
+  length: React.PropTypes.number.isRequired,
+  dashGap: React.PropTypes.number.isRequired,
+  dashLength: React.PropTypes.number.isRequired,
+  dashThickness: React.PropTypes.number.isRequired,
+  dashColor: React.PropTypes.string
+};
+
+Dash.defaultProps = {
+  dashGap: 2,
+  dashLength: 4,
+  dashThickness: 2,
+  dashColor: 'black'
+}
+
+module.exports = Dash;
