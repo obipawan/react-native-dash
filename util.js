ca868e8ad93f17038d89f52a4599fccacc7a6b75
Umair Ahmed <umair.ahmed@thedistance.co.uk>
2018-05-29 10:14:14 +0100

    updating const to let

--- util.js
+++ util.js
@@ -1,35 +1,43 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native';
 
-export const isStyleRow = (style) => {
-	const flatStyle = StyleSheet.flatten(style || {})
-	return flatStyle.flexDirection !== 'column'
-}
+export const isStyleRow = style => {
+  const flatStyle = StyleSheet.flatten(style || {});
+  return flatStyle.flexDirection !== 'column';
+};
 
-const getDashStyleId = ({ dashGap, dashLength, dashThickness, dashColor }, isRow) =>
-	`${dashGap}-${dashLength}-${dashThickness}-${dashColor}-${isRow ? 'row' : 'column'}`
+const getDashStyleId = (
+  { dashGap, dashLength, dashThickness, dashColor },
+  isRow
+) =>
+  `${dashGap}-${dashLength}-${dashThickness}-${dashColor}-${
+    isRow ? 'row' : 'column'
+  }`;
 
-const createDashStyleSheet = ({ dashGap, dashLength, dashThickness, dashColor }, isRow) => {
-	const idStyle = new StyleSheet.create({
-		style: {
-			width: isRow ? dashLength : dashThickness,
-			height: isRow ? dashThickness : dashLength,
-			marginRight: isRow ? dashGap : 0,
-			marginBottom: isRow ? 0 : dashGap,
-			backgroundColor: dashColor,
-		},
-	})
-	return idStyle.style
-}
+const createDashStyleSheet = (
+  { dashGap, dashLength, dashThickness, dashColor },
+  isRow
+) => {
+  const idStyle = new StyleSheet.create({
+    style: {
+      width: isRow ? dashLength : dashThickness,
+      height: isRow ? dashThickness : dashLength,
+      marginRight: isRow ? dashGap : 0,
+      marginBottom: isRow ? 0 : dashGap,
+      backgroundColor: dashColor,
+    },
+  });
+  return idStyle.style;
+};
 
-const stylesStore = {}
-export const getDashStyle = (props) => {
-	const isRow = isStyleRow(props.style)
-	const id = getDashStyleId(props, isRow)
-	if (!stylesStore[ id ]) {
-		stylesStore = {
-			...stylesStore,
-			[ id ]: createDashStyleSheet(props, isRow),
-		}
-	}
-	return stylesStore[ id ]
-}
+let stylesStore = {};
+export const getDashStyle = props => {
+  const isRow = isStyleRow(props.style);
+  const id = getDashStyleId(props, isRow);
+  if (!stylesStore[id]) {
+    stylesStore = {
+      ...stylesStore,
+      [id]: createDashStyleSheet(props, isRow),
+    };
+  }
+  return stylesStore[id];
+};
