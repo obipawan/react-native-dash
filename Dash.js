25367f138b0989a89feee8fb8e2940fd6ac0618e
Pawan <pawan.kumar1@myntra.com>
2019-08-31 17:14:40 +0530

    hotfix

--- Dash.js
+++ Dash.js
@@ -8,7 +8,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { View, StyleSheet, ViewPropTypes } from 'react-native'
 import MeasureMeHOC from 'react-native-measureme'
-import { getDashStyle, isStyleRow } from './util'
+import { getDashStyle, isStyleRow } from '../util'
 
 const Dash = props => {
 	const isRow = isStyleRow(props.style)
