47f384616e193a0f85a8ee38223e92469c54a108
Amaury Liet <amaury.liet@gmail.com>
2020-09-04 12:05:47 +0200

    Avoid overflowing last dash
    Avoid inconsistent component size between its first and second render

--- util.js
+++ util.js
@@ -8,21 +8,26 @@ export const isStyleRow = style => {
 const getDashStyleId = (
   { dashGap, dashLength, dashThickness, dashColor },
   isRow,
+  isShortened = false
 ) =>
   `${dashGap}-${dashLength}-${dashThickness}-${dashColor}-${
     isRow ? 'row' : 'column'
-  }`
+  }-${isShortened}`
 
 const createDashStyleSheet = (
   { dashGap, dashLength, dashThickness, dashColor },
   isRow,
+  maxLength,
 ) => {
+  const currentDashLength = maxLength === undefined ? dashLength : Math.min(maxLength, dashLength);
+  const currentGapLength = maxLength === undefined ? dashGap : Math.max(maxLength - dashLength, 0);
+
   const idStyle = StyleSheet.create({
     style: {
-      width: isRow ? dashLength : dashThickness,
-      height: isRow ? dashThickness : dashLength,
-      marginRight: isRow ? dashGap : 0,
-      marginBottom: isRow ? 0 : dashGap,
+      width: isRow ? currentDashLength : dashThickness,
+      height: isRow ? dashThickness : currentDashLength,
+      marginRight: isRow ? currentGapLength : 0,
+      marginBottom: isRow ? 0 : currentGapLength,
       backgroundColor: dashColor,
     },
   })
@@ -30,13 +35,13 @@ const createDashStyleSheet = (
 }
 
 let stylesStore = {}
-export const getDashStyle = props => {
+export const getDashStyle = (props, maxLength=undefined) => {
   const isRow = isStyleRow(props.style)
-  const id = getDashStyleId(props, isRow)
+  const id = getDashStyleId(props, isRow, maxLength !== undefined)
   if (!stylesStore[id]) {
     stylesStore = {
       ...stylesStore,
-      [id]: createDashStyleSheet(props, isRow),
+      [id]: createDashStyleSheet(props, isRow, maxLength),
     }
   }
   return stylesStore[id]
